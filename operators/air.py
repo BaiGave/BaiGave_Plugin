241c597eb11e8d5c1dd506065bd0b4e540af772b
BaiGave <107305554+BaiGave@users.noreply.github.com>
2023-10-01 09:42:25 +0800

    Update air.py

--- operators/air.py
+++ operators/air.py
@@ -1,4 +1,4 @@
 air_blocks = ["minecraft:air","minecraft:water","minecraft:brown_mushroom","minecraft:snow", "minecraft:spruce_leaves","minecraft:glass","minecraft:fern",
-              "minecraft:grass","minecraft:pointed_dripstone","minecraft:dead_bush","minecraft:crimson_fence",
+              "minecraft:grass","minecraft:pointed_dripstone","minecraft:dead_bush","minecraft:crimson_fence","minecraft:large_fern",
               "minecraft:oxidized_cut_copper_stairs","byg:pink_anemone","byg:white_anemone","byg:hydrangea_hedge",
               "byg:yellow_daffodil","byg:clover_patch","byg:lollipop_flower","byg:pink_allium"]
