6df5afbcee0dbd09e08cfe1ed1377716d32f8d2b
Piggestpig <69968293+piggestpig@users.noreply.github.com>
2023-10-20 12:33:56 +0800

    Add files via upload

--- operators/classification.py
+++ operators/classification.py
@@ -11,3 +11,10 @@ leaves =["minecraft:spruce_leaves","minecraft:acacia_leaves","minecraft:birch_le
 
 liquid = ["minecraft:water","minecraft:lava"]
 
+exclude = ["minecraft:air","minecraft:brown_mushroom","minecraft:fern","minecraft:large_fern","minecraft:grass","minecraft:dead_bush","byg:pink_anemone",
+            "byg:white_anemone","byg:hydrangea_hedge","byg:yellow_daffodil","byg:clover_patch","byg:crocus","byg:white_sage","byg:purple_sage","byg:lollipop_flower",
+            "byg:pink_allium","byg:black_rose","byg:winter_succulent","byg:white_puffball","minecraft:spruce_leaves","minecraft:acacia_leaves","minecraft:birch_leaves","minecraft:dark_oak_leaves","minecraft:jungle_leaves","minecraft:mangrove_leaves",
+         "minecraft:oak_leaves","minecraft:azalea_leaves","minecraft:flowering_azalea_leaves","byg:yellow_birch_leaves","byg:orange_birch_leaves","byg:fir_leaves","minecraft:water","minecraft:lava","minecraft:dirt","minecraft:grass_block",
+         "minecraft:stone","minecraft:deepslate","minecraft:sand","minecraft:gravel","minecraft:snow","minecraft:snow_block","minecraft:powder_snow"]
+
+
