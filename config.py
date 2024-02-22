4804e45d477c9c7b804fb0d4a780a7ab35fa6221
BaiGave <107305554+BaiGave@users.noreply.github.com>
2024-02-22 22:12:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 config={
     "resourcepack_list": [],
-    "mod_list": ["minecraft", "yuushya"],
+    "mod_list": ["minecraft", "yuushya", "additionalstructures", "repurposed_structures", "t_and_t"],
     "version":"1.20.1",
-    "save":"",
+    "save":"World1",
     "schem":"",
     "color":"minecraft_colors.py"
     }
